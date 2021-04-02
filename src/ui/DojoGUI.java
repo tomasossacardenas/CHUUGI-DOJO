e8b7e55828f3803ad67a59f3404b697d0d472891
Tomas Ossa <tomasossaefcsl@gmail.com>
2021-04-02 00:07:15 -0500

    Method to generate a pdf file has been done, has been added a new Library to generate pdf, and some screens have been done

--- src/ui/DojoGUI.java
+++ src/ui/DojoGUI.java
@@ -1,5 +1,7 @@
 package ui;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -13,6 +15,17 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.layout.Pane;
 import model.Dojo;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
 
 public class DojoGUI {
 	
@@ -49,6 +62,29 @@ public class DojoGUI {
     @FXML
     private Pane PaneOptionsWindow;
 
+// CreateStudent fxml
+    
+    @FXML
+    private Pane PaneCreateStudent;
+
+    @FXML
+    private TextField txtCreateStudentName;
+
+    @FXML
+    private TextField txtCreateStudentSurnames;
+
+    @FXML
+    private TextField txtCreateStudentId;
+
+    @FXML
+    private TextField txtCreateStudentPhoneContact;
+
+    @FXML
+    private TextField txtCreateStudentPayersName;
+
+    @FXML
+    private TextField txtCreateStudentEmail;
+
     @FXML
     void openLoginScreen(ActionEvent event) throws IOException {
 		FXMLLoader login = new FXMLLoader(getClass().getResource("Login.fxml"));
@@ -56,9 +92,25 @@ public class DojoGUI {
 		Parent rootLogin = login.load();
 		OptionsWindow.getChildren().setAll(rootLogin);
     }
+    
+    @FXML
+    void openCreateStudent(ActionEvent event) throws IOException {
+    	FXMLLoader fxml = new FXMLLoader(getClass().getResource("CreateStudent.fxml"));
+		fxml.setController(this);
+		Parent root= fxml.load();
+		PaneOptionsWindow.getChildren().setAll(root);
+    }
+    
+    @FXML
+    void openDeleteStudent(ActionEvent event) throws IOException {
+    	FXMLLoader fxml = new FXMLLoader(getClass().getResource("DeleteStudent.fxml"));
+		fxml.setController(this);
+		Parent root= fxml.load();
+		PaneOptionsWindow.getChildren().setAll(root);
+    }
 
     @FXML
-    void buttonSignIn(ActionEvent event) throws IOException {
+    public void buttonSignIn(ActionEvent event) throws IOException {
     	String dojoUserUsername= dojo.getUsuario().getUsername();
     	String dojoUserPassword= dojo.getUsuario().getPassword();
 
@@ -85,6 +137,95 @@ public class DojoGUI {
     		dialog.show();
     	}
 
+    }
+    
+    @FXML
+    void generatePDF(ActionEvent event) throws FileNotFoundException {//method of the button createStudent its TEMPORAL
+    	FileOutputStream archivo= new FileOutputStream("C:\\Users\\tomas\\eclipse-workspace\\jfx-ChuugiDojo\\Reportes\\"+txtCreateStudentId.getText()+".pdf");
+    	
+    	Document document= new Document();  
+    	
+    	try {//
+			PdfWriter.getInstance(document, archivo);
+			document.open();
+			addMetaData(document);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+    	document.close();
+    	
+    	
+    }
+    
+    private static void addMetaData(Document document) throws DocumentException {
+    	Image foto=null;
+   
+        try
+        {
+        	foto = Image.getInstance("C:\\Users\\tomas\\eclipse-workspace\\jfx-ChuugiDojo\\src\\icons\\CHUUGI JKA.jpg");
+        	foto.scaleToFit(200, 200);
+        	foto.setAlignment(Chunk.ALIGN_CENTER);
+        }
+        catch ( Exception e )
+        {
+        	e.printStackTrace();
+        }
+        
+        Paragraph p = new Paragraph("EDIFICIO LOS GUADUALES\n"
+        							+ "NIT:67007645-6\n"
+        							+ "LUZ EDITH ORTIZ C. \n"
+        							+ "info@chuugidojo.com \n"
+        							+ "Celular:(310 650 7454)-(313 559 2722) \n\n");
+        p.setFont(FontFactory.getFont("Tahoma", 18));
+        
+        PdfPTable table = new PdfPTable(2);
+        table.setWidthPercentage(100);
+        table.addCell(getCell(p.getContent(), PdfPCell.ALIGN_LEFT));
+        table.addCell(getCell(foto, PdfPCell.ALIGN_RIGHT));
+        document.add(table);
+        
+        document.add(new Paragraph(""));
+        document.add(new Paragraph(""));
+        document.add(new Paragraph(""));
+        document.add(new Paragraph(""));
+        document.add(new Paragraph(""));
+        
+        Paragraph parrafoPago = new Paragraph();
+        parrafoPago.add("FECHA: \n"+
+        				"RECIBÍ DE: \n"+
+        				"LA SUMA DE: \n");
+        parrafoPago.setFont(FontFactory.getFont("Tahoma", 18, Font.BOLD));
+        
+        Paragraph parrafoPagoR = new Paragraph();
+        parrafoPagoR.add("fecha Actual\n"+
+        				"Persona quien paga\n"+
+        				"Total que pagó \n");
+        parrafoPagoR.setFont(FontFactory.getFont("Tahoma", 18));
+        
+        PdfPTable tablePago = new PdfPTable(2);
+        tablePago.setWidthPercentage(100);
+        tablePago.addCell(getCell(parrafoPago.getContent(), PdfPCell.ALIGN_RIGHT));
+        tablePago.addCell(getCell(parrafoPagoR.getContent(), PdfPCell.ALIGN_LEFT));
+        document.add(tablePago);
+        
+        
+        
+        
+    }
+    public static PdfPCell getCell(String text, int alignment) {//configuracion de la celda del texto (izquierda)
+        PdfPCell cell = new PdfPCell(new Phrase(text));
+        cell.setPadding(0);
+        cell.setHorizontalAlignment(alignment);
+        cell.setBorder(PdfPCell.NO_BORDER);
+        return cell;
+    }
+    public static PdfPCell getCell(Image img, int alignment) {//configuracion de la celda de la imagen (derecha)
+        PdfPCell cell = new PdfPCell();
+        cell.addElement(img);
+        cell.setPadding(0);
+        cell.setHorizontalAlignment(alignment);
+        cell.setBorder(PdfPCell.NO_BORDER);
+        return cell;
     }
 
 }
